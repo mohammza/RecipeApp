5084326fa1cb7e44c679d4cb90a1b297469e0a77
mohammza <43731407+mohammza@users.noreply.github.com>
2018-09-30 17:15:26 -0400

    Add files via upload

---
+++ app/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component, Output } from '@angular/core';
+import { EventEmitter } from 'events';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html'
+})
+export class HeaderComponent {
+  @Output() featureSelected = new EventEmitter();
+
+  onSelect(feature: string) {
+    this.featureSelected.emit(feature);
+  }
+}
